f9b113eb7a6389d4b414cb0c5d4d358c8c789937
Sheriefs Gamble <sgamble284@gmail.com>
2020-02-29 14:05:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reversename/:name'
+    @reversed_name = params[:name]..reverse
+    "#{reversed_name}"
 end
